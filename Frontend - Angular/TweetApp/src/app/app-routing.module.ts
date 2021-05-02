456542e035d075c619f8264aa1a099f38249018f
karunjossy333 <karunjossy333@gmail.com>
2021-05-02 16:58:38 +0530

    Completed frontend template

--- Frontend - Angular/TweetApp/src/app/app-routing.module.ts
+++ Frontend - Angular/TweetApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoggedInUserModule } from './logged-in-user/logged-in-user.module';
+import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginDashboardComponent},
+  { path: 'user', loadChildren: './logged-in-user/logged-in-user.module#LoggedInUserModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend - Angular/TweetApp/src/app/app.module.ts
+++ Frontend - Angular/TweetApp/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { HttpClientModule } from '@angular/common/http';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SelectButtonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    InputMaskModule,
+    HttpClientModule,
+    DialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
